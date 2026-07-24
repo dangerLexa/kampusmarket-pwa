09ec565ceca298d2b67678b76275d6b22b82a78d
dangerLexa <alexshishkov1304@gmail.com>
2026-07-24 18:32:35 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -21,9 +21,7 @@ const ASSETS = [
 ];
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 self.addEventListener('fetch', (event) => {
